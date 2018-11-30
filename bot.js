85175304cb38b4f9af58b4f92bcbdce0a198cbe2
yyyTak2 <45369491+yyyTak2@users.noreply.github.com>
2018-11-30 01:30:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ bot.on('ready', () => {
     bot.user.setStatus('dnd')
     bot.user.setPresence({
         game: {
-            name: '〽️ Bot do Testów 〽️',
+            name: '〽️Bot do Testów〽️',
             type: 0
         }
     });

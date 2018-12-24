fbab0b356d1ab6d28fe4963c057206da9eff82ba
Dominik <45369491+yyyTak2@users.noreply.github.com>
2018-12-24 23:39:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ bot.on('ready', () => {
     bot.user.setStatus('dnd')
     bot.user.setPresence({
         game: {
-            name: '〽️Bot do Testów〽️',
+            name: 'Wesołych świąt!🎄🎁',
             type: 0
         }
     });

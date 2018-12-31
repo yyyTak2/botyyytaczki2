b3185038725c498152b577bbe293a45395d4d10d
Dominik <45369491+yyyTak2@users.noreply.github.com>
2018-12-31 19:38:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ bot.on('ready', () => {
     bot.user.setStatus('dnd')
     bot.user.setPresence({
         game: {
-            name: 'Huehuehue',
+            name: 'Szczęsliwego Nowego Roku! 🎆🥂',
             type: 0
         }
     });
